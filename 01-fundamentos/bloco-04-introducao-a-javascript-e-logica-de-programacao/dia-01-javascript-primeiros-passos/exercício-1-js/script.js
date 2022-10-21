5cad7e2e0e265e6e9542c31d01ad8ab5b30ff9d4
rodrigo <rodrigoyasha@hotmail.com>
2022-10-21 13:51:02 -0300

    exercicio dia 1 js

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercício-1-js/script.js
@@ -0,0 +1,13 @@
+const myName = "rodrigo";
+const birthCity = "campinaGrande";
+const birthYear = 1987
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+console.log(birthYear);
+
+birthCity = "recife";
+console.log(birthCity);
